74c034ebddfe6dd66b8008d852535fef6b9c58a6
ozzy <osbaldogonzalezislc@gmail.com>
2021-01-29 11:20:42 -0800

    Fixed Teens
    I previously only had the code working for the ten and no other teen completly! Now it works, and the code should be simpler and easier to understand as well!

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import sys
 
 def main_output(num):
 	if not num: #edge case where the number is == 0
-		print(num, "=>", "zero")
+		format_output(0, "zero")
 	else:
 		digits = num2digits(num)
-		print(digits2word(digits))
+		format_output(num, digits2word(digits))
 
 def num2digits(num):
 	power = int(math.log10(num)) #how many digits in num (minus 1 since start count at 0)
@@ -18,8 +18,12 @@ def num2digits(num):
 	return each_digit
 
 def digits2word(digits):
-	weird_conv = {2: 'twen', 3: 'thir', 5: 'fif', 8: 'eigh'} #not eightty its eighty
-	normal_base = {1 : "one", 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 
+	teens_conv = {0: "ten", 1: "eleven", 2: "twelve", 3: "thirteen", 4: "fourteen",
+				  5: "fifteen", 6: "sixteen", 7: "seventeen", 8: "eighteen", 
+				  9: "nineteen"}
+	conv_10s = {2: "twenty", 3: "thirty", 4: "fourty", 5: "fifty", 6: "sixty", 
+				7 : "seventy", 8: "eighty", 9: "ninety"}
+	normal_base = {1: "one", 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 
 		   		   8: 'eight', 9: 'nine'}
 	super_sufix = ["", "", "thousand", "million", "billion", "trillion", "quadrillion", 
 				   "quintrillion", "sextillion", "septillion", "octillion", "nonillion",
@@ -32,42 +36,37 @@ def digits2word(digits):
 
 	for i in range(out_loop_num):
 		digitslice = digits[3*i:3*i+3]
+		skip_next = False
 		for j, num in enumerate(digitslice):
-			if num == 0:
+			if skip_next or num == 0: #there is no word related with zero 
 				continue 
-
+		
 			if j == 0:
-				prefix = normal_base[num]
-				sufix = "-hundred"
+				prefix = normal_base[num] + "-hundred"
 
 			elif j == 1: #currently in the 10s place
-				#prefix
-				if (digitslice[j]*10 + digitslice[j+1]) == 10:
-					prefix = "ten"
-				elif num in weird_conv: #part of group of prefixes that change: 2, 3, 5
-					prefix = weird_conv[num]
-				else: #normal 10s place
-					prefix = normal_base[num]
-				
-				#sufix teen of ty
-				if digitslice[j+1]: #if digitslice == 0 then current num is 10
-					if num == 1:
-						sufix = "teen"
-					else:
-						sufix = "ty"
-				else: #sufix for 10
-					sufix = ""
-				
+				if num == 1: #all the teens are here
+					prefix = teens_conv[digitslice[j+1]]
+					skip_next = True
+				else:
+					prefix = conv_10s[num]
+
 			else: #j == 2 1s place
 				prefix = normal_base[num]
-				sufix = "" #no sufix for the 1s place!
 
-			output_text += " " + prefix + sufix
+			output_text += " " + prefix
 		modifier = super_sufix[out_loop_num - i]
 		output_text += " " + modifier
 	return output_text
 
-main_output(110_710)
+def format_output(og_num, digit_word):
+	print(og_num, "=>", digit_word)
+
+if len(sys.argv) > 1 and len(sys.argv) < 3:
+	main_output(int(sys.argv[1]))
+else:
+	main_output(int(input("Please provide a number: ")))
+
 
 #Ivan
 # num2wrd = {1: 'One', 2: 'Two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 
